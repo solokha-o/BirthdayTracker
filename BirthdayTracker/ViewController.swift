f020396a8013a9d15aaddccc185a8766afa10dba
Oleksandr Solokha <oleksandrsolokha@MacBook-Air-Oleksandr.local>
2020-01-08 12:13:48 +0200

    Initial Commit

---
+++ BirthdayTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BirthdayTracker
+//
+//  Created by Oleksandr Solokha on 08.01.20.
+//  Copyright © 2020 Oleksandr Solokha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
